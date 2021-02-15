e5f704ac5c08282675015db7bc1e673387b912f9
Antonio Victor Ribeiro Morelli <victor.morelli.007@gmail.com>
2021-02-15 13:15:48 -0300

    stupid coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,23 @@
 class QuestionsController < ApplicationController
+  def ask; end
+
+  def answer
+    @user_input = params[:ask]
+    @coach_answer = coach(@user_input)
+  end
+
+  def coach(user_input)
+    coach_answers = {
+      "I am going to work": 'Great!',
+      "?": 'Silly question, get dressed and go to work!',
+      "default": "I don't care, get dressed and go to work!"
+    }
+    if coach_answers.key?(user_input.to_sym)
+      coach_answers[user_input.to_sym]
+    elsif coach_answers.key?(user_input[-1].to_sym)
+      coach_answers[user_input[-1].to_sym]
+    else
+      coach_answers[:default]
+    end
+  end
 end
